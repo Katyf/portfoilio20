647664f569c9cc2abf9480682f192de59eaeba09
katy <katy.feng.14@gmail.com>
2020-07-20 12:14:18 -0400

    Updates

---
+++ precache-manifest.52f335dacdc412daaa897a2b1ef99938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9beb9f52ec3477bec65e73308e94ad9",
+    "url": "/portfoilio20/index.html"
+  },
+  {
+    "revision": "726c5fe5d56c0ff7a2df",
+    "url": "/portfoilio20/static/css/main.e954b62a.chunk.css"
+  },
+  {
+    "revision": "ee5e49ca672c6e777ddc",
+    "url": "/portfoilio20/static/js/2.078d7a09.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfoilio20/static/js/2.078d7a09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "726c5fe5d56c0ff7a2df",
+    "url": "/portfoilio20/static/js/main.b7922a1f.chunk.js"
+  },
+  {
+    "revision": "fa7e1b45998f2be798b7",
+    "url": "/portfoilio20/static/js/runtime-main.8bf3d56d.js"
+  }
+]);
